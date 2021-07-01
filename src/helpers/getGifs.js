8904c9c07fec90e84e82800a834fbcf673c57b0c
bettyarellano <bettyarellano@gmail.com>
2021-07-01 16:38:15 -0300

    https y limit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,18 +1,16 @@
 
 export const getGifs = async ( category ) => {
-    const url      = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=tbfLuIWiHvfW85HcduTGXpeoXfklP4SS`
+    const url      = `https://api.giphy.com/api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=12&api_key=tbfLuIWiHvfW85HcduTGXpeoXfklP4SS`
     const resp     = await fetch(url);
     const { data } = await resp.json();
 
 
-    const gifs = data.map( img => {
-    
+    const gifs = data.map( img => { 
         return {
             id:    img.id,
             title: img.title,
             url:   img.images?.downsized_medium.url,
         }
     })
-
     return gifs;
 }
